68c147c02bae6911dd0b1353dad727a27fe65355
CrazyElfi <al.gaffarova.26@gmail.com>
2019-06-26 00:46:45 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(function(){
+    console.log('hdjskdl')
+    $('#content_subitems').masonry({
+        // options
+        itemSelector : '.project_item',
+        columnWidth : 0,
+        isAnimated: true,
+        fitWidth: true,
+        isFitWidth: true
+    });
+});
